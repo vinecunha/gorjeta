ba2cb95614abda559c10cec12dd216dbfdc9be53
vinecunha <tec.viniciuscunha@gmail>
2023-06-24 13:01:00 -0300

    v.1.0.0.2

--- src/Footer.js
+++ src/Footer.js
@@ -7,9 +7,9 @@ export default function Footer() {
             <p className='text-center m-0'>Developed by @vinecunha with PrimeReact and Bootstrap</p>
             <div className="d-flex flex-row justify-content-center align-items-center mb-1">
                 <a href='https://vinecunha.github.io' className='mx-1' style={{textDecoration: "none", color: "#fff"}} target='blank'>vinecunha.github.io</a> |
-                <a href="https://linkedin.com/in/vcmartins" className=" mx-1 link-info" target="blank">Linkedin</a> |
-                <a href="https://github.com/vinecunha" className=" mx-1 link-info" target="blank">GitHub</a> |
-                <a href="mailto:vcunha@id.uff.br" className=" mx-1 link-info">E-mail</a>
+                <a href="https://linkedin.com/in/vcmartins" className=" mx-1 link-warning" target="blank">Linkedin</a> |
+                <a href="https://github.com/vinecunha" className=" mx-1 link-warning" target="blank">GitHub</a> |
+                <a href="mailto:vcunha@id.uff.br" className=" mx-1 link-warning">E-mail</a>
             </div>
         </div>
     );
